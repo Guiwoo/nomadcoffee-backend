f79862824b1b4992f3d6ecce0c4628717a46f8a5
Guiwoo <park.guiwoo@hotmail.com>
2021-08-12 19:12:01 +0900

    playground

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 import { getUser, protectResolver } from "./users/utils";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
 const PORT = process.env.PORT;
 
 const server = new ApolloServer({
   schema,
-  playground: true,
   introspection: true,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
   context: async ({ req, res }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
