701a9209365727c40b3a049e6369853d8818598f
Guiwoo <park.guiwoo@hotmail.com>
2021-08-18 19:17:51 +0900

    Change page number

--- src/coffeeShop/seeCoffeeShops/seeCoffeeShops.resolvers.js
+++ src/coffeeShop/seeCoffeeShops/seeCoffeeShops.resolvers.js
@@ -3,6 +3,6 @@ import client from "../../client";
 export default {
   Query: {
     seeCoffeeShops: (_, { page }) =>
-      client.coffeeShop.findMany({ skip: (page - 1) * 3, take: 3 }),
+      client.coffeeShop.findMany({ skip: (page - 1) * 4, take: 4 }),
   },
 };
