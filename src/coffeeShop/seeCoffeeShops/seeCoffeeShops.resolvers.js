390fd4440fbc410d3764af269361c3362e610cfc
Guiwoo <park.guiwoo@hotmail.com>
2021-08-31 13:57:10 +0900

    orderby

--- src/coffeeShop/seeCoffeeShops/seeCoffeeShops.resolvers.js
+++ src/coffeeShop/seeCoffeeShops/seeCoffeeShops.resolvers.js
@@ -3,6 +3,12 @@ import client from "../../client";
 export default {
   Query: {
     seeCoffeeShops: (_, { page }) =>
-      client.coffeeShop.findMany({ skip: (page - 1) * 4, take: 4 }),
+      client.coffeeShop.findMany({
+        skip: (page - 1) * 4,
+        take: 4,
+        orderBy: {
+          id: "desc",
+        },
+      }),
   },
 };
